5bbf27474ef2b4a1fd7a922b20781485af2f4967
vladimir <napoleon520@gmail.com>
2020-12-18 22:11:43 +0200

    added checking for userName value during creating account(to avoid empty value) and disabled submit button if form has some empty value

--- src/Components/FormControls/LoginForm.tsx
+++ src/Components/FormControls/LoginForm.tsx
@@ -11,103 +11,105 @@ import { FormHOC } from "../HOC/formHOC";
 import { compose } from "redux";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        "& > *": {
-            margin: theme.spacing(1),
-            width: "90%",
-        },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "90%",
     },
+  },
 }));
 
 interface LoginFormTypes {
-    formik: any;
-    changePage: () => void;
-    BlockedButtonArray: Array<number | string>;
-    setCreateOrLog: (value: boolean) => void;
+  formik: any;
+  changePage: () => void;
+  BlockedButtonArray: Array<number | string>;
+  setCreateOrLog: (value: boolean) => void;
 }
 
 const LoginForm: React.FC<LoginFormTypes> = ({
-    formik,
-    changePage,
-    BlockedButtonArray,
-    setCreateOrLog,
+  formik,
+  changePage,
+  BlockedButtonArray,
+  setCreateOrLog,
 }) => {
-    const [state, setState] = React.useState({
-        showPass: false,
-    });
-    const classes: any = useStyles();
-    return (
-        <form className="loginForm" onSubmit={formik.handleSubmit}>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-                Log in
-            </div>
-            <div className="inputTaskDiv">
-                {formik.errors.email ? (
-                    <ErrorValidate
-                        name="email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        label={formik.errors.email}
-                    />
-                ) : (
-                    <div className="formBlock">
-                        <TextField
-                            label="email"
-                            type="text"
-                            name="email"
-                            onChange={formik.handleChange}
-                            value={formik.values.email}
-                            autoFocus={false}
-                            className="loginField"
-                        />
-                    </div>
-                )}
-                <div style={{ position: "relative" }}>
-                    {formik.errors.password ? (
-                        <div className="formBlock">
-                            <Checkbox setState={setState} state={state} />
-                            <ErrorValidatePass
-                                label={formik.errors.password}
-                                name="password"
-                                onChange={formik.handleChange}
-                                value={formik.values.password}
-                                state={state}
-                            />
-                        </div>
-                    ) : (
-                        <div className="formBlock">
-                            <Checkbox setState={setState} state={state} />
-                            <TextField
-                                label="password"
-                                type={state.showPass ? "text" : "password"}
-                                name="password"
-                                onChange={formik.handleChange}
-                                value={formik.values.password}
-                                autoFocus={false}
-                                className="passField"
-                            />
-                        </div>
-                    )}
-                </div>
-            </div>
-            <ChangePageButton
-                buttonName="Create Account"
-                changePage={changePage}
-                setCreateOrLog={setCreateOrLog}
+  const [state, setState] = React.useState({
+    showPass: false,
+  });
+  const classes: any = useStyles();
+  return (
+    <form className="loginForm" onSubmit={formik.handleSubmit}>
+      <div style={{ display: "flex", justifyContent: "center" }}>Log in</div>
+      <div className="inputTaskDiv">
+        {formik.errors.email ? (
+          <ErrorValidate
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            label={formik.errors.email}
+          />
+        ) : (
+          <div className="formBlock">
+            <TextField
+              label="email"
+              type="text"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              autoFocus={false}
+              className="loginField"
             />
-            <Button
-                type="submit"
-                style={{ margin: "0 auto" }}
-                variant="contained"
-                color="primary"
-                className={classes.button}
-                endIcon={<CloudUploadIcon />}
-                disabled={BlockedButtonArray.some((id) => id === "login")}
-            >
-                Send
-            </Button>
-        </form>
-    );
+          </div>
+        )}
+        <div style={{ position: "relative" }}>
+          {formik.errors.password ? (
+            <div className="formBlock">
+              <Checkbox setState={setState} state={state} />
+              <ErrorValidatePass
+                label={formik.errors.password}
+                name="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                state={state}
+              />
+            </div>
+          ) : (
+            <div className="formBlock">
+              <Checkbox setState={setState} state={state} />
+              <TextField
+                label="password"
+                type={state.showPass ? "text" : "password"}
+                name="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                autoFocus={false}
+                className="passField"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      <ChangePageButton
+        buttonName="Create Account"
+        changePage={changePage}
+        setCreateOrLog={setCreateOrLog}
+      />
+      <Button
+        type="submit"
+        style={{ margin: "0 auto" }}
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        endIcon={<CloudUploadIcon />}
+        disabled={
+          BlockedButtonArray.some((id) => id === "login") ||
+          !formik.values.email ||
+          !formik.values.password
+        }
+      >
+        Send
+      </Button>
+    </form>
+  );
 };
 
 export default compose(FormHOC)(LoginForm);
