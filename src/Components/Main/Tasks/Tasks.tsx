9a650fff74bfa6c17c198ea7ad12cd037c2dfd0a
vladimir <napoleon520@gmail.com>
2020-12-25 10:31:55 +0200

    changed add button color, and changed login, createacc, auth functions - added email setting and getting

--- src/Components/Main/Tasks/Tasks.tsx
+++ src/Components/Main/Tasks/Tasks.tsx
@@ -15,144 +15,143 @@ import Confirm from "../../CommonComponents/Confirm";
 const AddTask = React.lazy(() => import("./AddTask"));
 
 const useStyles = makeStyles({
-    root: {
-        width: "100%",
-    },
+  root: {
+    width: "100%",
+  },
 });
 
 const Tasks: React.FC<TasksFCTypes> = React.memo(
-    ({
-        TasksArray,
-        DeleteTaskThunk,
-        isAuth,
-        filterArray,
-        user,
-        loading,
-        BlockedButtonArray,
-        taskPanel,
-        setTaskPanel,
-    }) => {
-        const classes = useStyles();
-        let [addTask, changeAddTask] = useState(false);
-        const AddTaskFunc = () => {
-            addTask ? changeAddTask(false) : changeAddTask(true);
-        };
-        /// this code is responsible for showing modal window with questions - if you want to delete Task, and to delete it if yes
-        let [confirm, setValuesForDeleting]: any = useState({
-            value: false,
-            deleteId: null,
-            deleteKey: null,
-        });
-        const setConfirm = (
-            value: boolean,
-            id?: number | null,
-            key?: string | null
-        ) => {
-            setValuesForDeleting({
-                value: value,
-                deleteId: id,
-                deleteKey: key,
-            });
-        };
-        //----------------------------------
-        // function which sets animated css style to the item and then delete the task once animation finished
-        const DeleteTask = async (iD: number, keyFirebase: string) => {
-            setTaskPanel(iD);
-            setTimeout(async () => {
-                await DeleteTaskThunk(iD, keyFirebase);
-                setTaskPanel(iD);
-            }, 1100);
-        };
-        ///this will show or hide ADD TASK form
-        let [addTaskPanel, setAddTaskPanel] = useState("addTaskPanel");
-        if (loading) {
-            return <Preloader />;
-        }
-        return (
-            <>
-                {isAuth ? (
-                    <div className={classes.root}>
-                        <div className="userInfo">
-                            <div>Hello:</div>
-                            <div className="userInfoName">{user.name}</div>
-                        </div>
-                        <AddTaskButton
-                            AddTaskFunc={AddTaskFunc}
-                            setAddTaskPanel={setAddTaskPanel}
-                            addTaskPanel={addTaskPanel}
-                        />
-                        <div style={{ position: "relative", top: "-80px" }}>
-                            <Filter filterArray={filterArray} />
-                        </div>
-                        <div className={addTaskPanel}>
-                            {addTask ? (
-                                <Suspense fallback={<Preloader />}>
-                                    {" "}
-                                    <AddTask
-                                        changeAddTask={changeAddTask}
-                                        BlockedButtonArray={BlockedButtonArray}
-                                    />
-                                </Suspense>
-                            ) : null}
-                            {confirm.value ? (
-                                <Confirm
-                                    open={confirm.value}
-                                    setConfirm={setConfirm}
-                                    DeleteTask={DeleteTask}
-                                    deleteId={confirm.deleteId}
-                                    deleteKey={confirm.deleteKey}
-                                />
-                            ) : null}
-                            {TasksArray.map(
-                                ({
-                                    id,
-                                    text,
-                                    priority,
-                                    data,
-                                    status,
-                                    keyFirebase,
-                                    settedTime,
-                                    settedDate,
-                                }) => (
-                                    <div key={id}>
-                                        <TaskItemContainer
-                                            id={id}
-                                            text={text}
-                                            priority={priority}
-                                            status={status}
-                                            keyFirebase={keyFirebase}
-                                            data={data}
-                                            BlockedButtonArray={
-                                                BlockedButtonArray
-                                            }
-                                            settedTime={settedTime}
-                                            settedDate={settedDate}
-                                            TasksArray={TasksArray}
-                                            setConfirm={setConfirm}
-                                            taskPanel={taskPanel}
-                                            setTaskPanel={setTaskPanel}
-                                        />
-                                    </div>
-                                )
-                            )}
-                        </div>
-                    </div>
-                ) : null}
-            </>
-        );
+  ({
+    TasksArray,
+    DeleteTaskThunk,
+    isAuth,
+    filterArray,
+    user,
+    loading,
+    BlockedButtonArray,
+    taskPanel,
+    setTaskPanel,
+  }) => {
+    const classes = useStyles();
+    let [addTask, changeAddTask] = useState(false);
+    const AddTaskFunc = () => {
+      addTask ? changeAddTask(false) : changeAddTask(true);
+    };
+    /// this code is responsible for showing modal window with questions - if you want to delete Task, and to delete it if yes
+    let [confirm, setValuesForDeleting]: any = useState({
+      value: false,
+      deleteId: null,
+      deleteKey: null,
+    });
+    const setConfirm = (
+      value: boolean,
+      id?: number | null,
+      key?: string | null
+    ) => {
+      setValuesForDeleting({
+        value: value,
+        deleteId: id,
+        deleteKey: key,
+      });
+    };
+    //----------------------------------
+    // function which sets animated css style to the item and then delete the task once animation finished
+    const DeleteTask = async (iD: number, keyFirebase: string) => {
+      setTaskPanel(iD);
+      setTimeout(async () => {
+        await DeleteTaskThunk(iD, keyFirebase);
+        setTaskPanel(iD);
+      }, 1100);
+    };
+    ///this will show or hide ADD TASK form
+    let [addTaskPanel, setAddTaskPanel] = useState("addTaskPanel");
+    if (loading) {
+      return <Preloader />;
     }
+    return (
+      <>
+        {isAuth ? (
+          <div className={classes.root}>
+            <div className="userInfo">
+              <div className="userInfoName">
+                Hi {user.name}, <p className="email">'{user.email}'</p>
+              </div>
+            </div>
+            <AddTaskButton
+              AddTaskFunc={AddTaskFunc}
+              setAddTaskPanel={setAddTaskPanel}
+              addTaskPanel={addTaskPanel}
+            />
+            <div style={{ position: "relative", top: "-80px" }}>
+              <Filter filterArray={filterArray} />
+            </div>
+            <div className={addTaskPanel}>
+              {addTask ? (
+                <Suspense fallback={<Preloader />}>
+                  {" "}
+                  <AddTask
+                    changeAddTask={changeAddTask}
+                    BlockedButtonArray={BlockedButtonArray}
+                  />
+                </Suspense>
+              ) : null}
+              {confirm.value ? (
+                <Confirm
+                  open={confirm.value}
+                  setConfirm={setConfirm}
+                  DeleteTask={DeleteTask}
+                  deleteId={confirm.deleteId}
+                  deleteKey={confirm.deleteKey}
+                />
+              ) : null}
+              {TasksArray.map(
+                ({
+                  id,
+                  text,
+                  priority,
+                  data,
+                  status,
+                  keyFirebase,
+                  settedTime,
+                  settedDate,
+                }) => (
+                  <div key={id}>
+                    <TaskItemContainer
+                      id={id}
+                      text={text}
+                      priority={priority}
+                      status={status}
+                      keyFirebase={keyFirebase}
+                      data={data}
+                      BlockedButtonArray={BlockedButtonArray}
+                      settedTime={settedTime}
+                      settedDate={settedDate}
+                      TasksArray={TasksArray}
+                      setConfirm={setConfirm}
+                      taskPanel={taskPanel}
+                      setTaskPanel={setTaskPanel}
+                    />
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+        ) : null}
+      </>
+    );
+  }
 );
 const mapStateToProps = (state: AppStoreReducer) => ({
-    TasksArray: state.tasks.TasksArray,
-    isAuth: state.auth.isAuth,
-    loading: state.tasks.loading,
-    BlockedButtonArray: state.tasks.BlockedButtonArray,
-    taskPanel: state.alert.taskPanel,
-    user: state.auth.user,
+  TasksArray: state.tasks.TasksArray,
+  isAuth: state.auth.isAuth,
+  loading: state.tasks.loading,
+  BlockedButtonArray: state.tasks.BlockedButtonArray,
+  taskPanel: state.alert.taskPanel,
+  user: state.auth.user,
 });
 
 export default connect(mapStateToProps, {
-    DeleteTaskThunk,
-    filterArray,
-    setTaskPanel,
+  DeleteTaskThunk,
+  filterArray,
+  setTaskPanel,
 })(Tasks);
